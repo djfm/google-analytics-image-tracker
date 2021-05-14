0f7850c937bbc02dab677a806da4b341c01b186b
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-05-15 00:32:46 +0200

    basic infrastructure for tracking

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import path from 'path';
-import express from 'express';
+import express, { Request } from 'express';
+import crypto from 'crypto';
 
 import rootPath from '../../rootPath';
 
@@ -14,6 +15,41 @@ const publicPath = path.join(
 );
 
 app.use(express.static(publicPath));
+
+/*
+accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*\/*; q = 0.8'
+'x-forwarded-for': '10.56.111.59',
+'cache-control': 'max-age=259200',
+via: 'HTTP/1.1 github-camo (07ab9467), 1.1 network-proxy-180b0e5.ash1-iad.github.net (squid/3.5.23)'
+*/
+
+const unknownMeasurementId = 'unknownMeasurementId';
+
+const getId = (req: Request): string => {
+  if (req.hostname) {
+    return req.hostname;
+  }
+
+  return req.ip;
+};
+
+app.get('*', (req, res) => {
+  const measurementId = req.params.measurementId ?? unknownMeasurementId;
+
+  const sha = crypto.createHash('sha1');
+  sha.update(`${measurementId}-${getId(req)}`);
+
+  const ETag = sha.digest('hex');
+
+  // eslint-disable-next-line no-console
+  console.log('#### Request', req.url, '\n', req.query, '\n', req.headers, '\n', { measurementId });
+  res.set({
+    'Cache-Control': 'max-age=1',
+    'Content-Type': 'image/png',
+    ETag,
+  });
+  res.sendFile(path.join(publicPath, 'badge.png'));
+});
 
 // the process.env.PORT will be set by heroku
 const port = process.env.PORT ?? 3000;
