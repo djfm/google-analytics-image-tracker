8bf603022049d6aadb6cb4c3a047322f574f5aae
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-05-14 12:27:39 +0200

    added minimal server with express

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,20 @@
-console.log('hi');
+import path from 'path';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static(
+  path.resolve(
+    __dirname,
+    '..',
+    '..',
+    'public',
+  ),
+));
+
+// the process.env.PORT will be set by heroku
+const port = process.env.PORT ?? 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
